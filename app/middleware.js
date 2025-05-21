cc350847bd55e61d05ebfc93c8fe040b8942e979
송현서 <songmin21s@songhyeonseoui-MacBookPro.local>
2025-05-21 16:38:12 +0900

    asdf

--- app/middleware.js
+++ app/middleware.js
@@ -10,7 +10,7 @@ export const applyMiddlewareSupabaseClient = async (request) => {
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    process.env.NEXT_SUPABASE_ANON_KEY,
     {
       cookies: {
         getAll() {
