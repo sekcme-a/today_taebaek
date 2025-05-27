de756edb39e6fb6764a25c5a3d35e0fd35f07ac8
송현서 <songmin21s@songhyeonseoui-MacBookPro.local>
2025-05-27 21:45:01 +0900

    asdf

--- app/admin/routine/components/TodayTaebaek.js
+++ app/admin/routine/components/TodayTaebaek.js
@@ -6,7 +6,7 @@ const TodayTaebaek = () => {
       <h1 className="font-bold text-2xl">투데이 태백 이메일 보도기사 업로드</h1>
 
       <p className="mt-10 text-lg">
-        1. 다음 메일에서 전날의 보도자료들을 다운로드한다.
+        {`1. 다음 메일에서 전날의 보도자료들을 다운로드한다.`}
       </p>
       <Button
         onClick={() => window.open("https://mail.daum.net/top/F_138", "_blank")}
@@ -30,7 +30,7 @@ const TodayTaebaek = () => {
         4. 이미지들을 드래그해서 업로드 후, 추출 버튼 클릭
       </p>
       <p className="mt-10 text-lg">
-        5. 추출된 결과 확인 후, "일괄 저장하기" 클릭
+        {`5. 추출된 결과 확인 후, "일괄 저장하기" 클릭`}
       </p>
       <p className="mt-10 text-lg">
         6. 기사 목록에 들어가서 카테고리 등 선택 후 게재.
