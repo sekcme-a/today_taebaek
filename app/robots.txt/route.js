249f4cf59fe90437326ec33ef58ceaa1c5359ac2
송현서 <songmin21s@songhyeonseoui-MacBookPro.local>
2025-05-30 11:35:39 +0900

    asdf

--- app/robots.txt/route.js
+++ app/robots.txt/route.js
@@ -3,18 +3,11 @@ import { NextResponse } from "next/server";
 export function GET() {
   const content = `
 User-agent: *
+Allow: /
 Disallow: /admin
 Disallow: /admin/
 Disallow: /admin-login
 Disallow: /admin-login/
-
-User-agent: GPTBot
-Disallow: /
-
-User-agent: DeepSeekBot
-Disallow: /
-
-Allow: /
 
 Sitemap: https://xn--2n1b19ndwjhoj6sb.com/sitemap.xml
   `.trim();
